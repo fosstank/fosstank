45ac7598f4042d5de368f812a11847af546dbe90
Brody Eller <ellerbrody@gmail.com>
2025-07-14 23:39:56 -0400

    Set up pocketbase

--- control-plane/main.go
+++ control-plane/main.go
@@ -5,11 +5,15 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"os/signal"
 	"regexp"
 	"strings"
 	"sync"
-	"syscall"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+
+	_ "control-plane/migrations"
 )
 
 func main() {
@@ -18,36 +22,57 @@ func main() {
 	// 2. Call ffmpeg cli to convert input from input format to HLS fmp4 AV1
 	// 3. Send HLS to CDN. Send camera name and playlist url(in CDN) to remote server
 
-	config := &Config{}
-	config.LoadFromFile("config.json")
+	app := pocketbase.New()
 
-	const output_dir = "streams"
-	err := os.MkdirAll(output_dir, 0755)
-	if err != nil {
-		log.Fatalf("failed to create streams directory: %v", err)
-	}
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Dashboard
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
 
 	ctx, cancel := context.WithCancel(context.Background())
 	var wg sync.WaitGroup
-	for _, streamConfig := range config.Streams {
-		wg.Add(1)
-		go encodeStream(ctx, &wg, output_dir, streamConfig)
+	app.OnServe().BindFunc(func(e *core.ServeEvent) error {
+		streams, err := app.FindAllRecords("streams")
+		if err != nil {
+			return err
+		}
+
+		const output_dir = "streams"
+		err = os.MkdirAll(output_dir, 0755)
+		if err != nil {
+			return err
+		}
+
+		for _, stream := range streams {
+			wg.Add(1)
+			go encodeStream(ctx, &wg, output_dir, stream)
+		}
+
+		return e.Next()
+	})
+
+	app.OnTerminate().BindFunc(func(e *core.TerminateEvent) error {
+		cancel()
+		wg.Wait()
+		return e.Next()
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
 	}
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	sig := <-sigChan
-	log.Println("Received signal ", sig)
-
-	cancel()
-	wg.Wait()
 }
 
-func encodeStream(ctx context.Context, wg *sync.WaitGroup, outputDir string, sc StreamConfig) error {
+func encodeStream(ctx context.Context, wg *sync.WaitGroup, outputDir string, stream *core.Record) error {
 	defer wg.Done()
 
-	subfolder := strings.ReplaceAll(sc.Name, " ", "_")
+	name := stream.GetString("name")
+	rtspUrl := stream.GetString("rtspUrl")
+
+	subfolder := strings.ReplaceAll(name, " ", "_")
 	re := regexp.MustCompile(`[^a-zA-Z0-9._-]`)
 	subfolder = re.ReplaceAllString(subfolder, "")
 	subfolder = strings.TrimLeft(subfolder, ".")
@@ -61,7 +86,7 @@ func encodeStream(ctx context.Context, wg *sync.WaitGroup, outputDir string, sc 
 		"ffmpeg",
 		"-loglevel", "error",
 		"-rtsp_transport", "tcp",
-		"-i", sc.RTSPUri,
+		"-i", rtspUrl,
 		"-c:a", "aac",
 		"-s", "hd1080",
 		"-c:v", "libsvtav1",
@@ -73,7 +98,7 @@ func encodeStream(ctx context.Context, wg *sync.WaitGroup, outputDir string, sc 
 		"-hls_list_size", "5",
 		"-hls_segment_type", "fmp4",
 		"-hls_flags", "delete_segments",
-		outputDir+"/"+subfolder+"/"+sc.Name+".m3u8",
+		outputDir+"/"+subfolder+"/"+name+".m3u8",
 	)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr

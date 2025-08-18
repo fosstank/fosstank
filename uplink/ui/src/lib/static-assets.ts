1db3054bd6c1ad25dd0a95e0d1db397a759032db
Pierre Morrel <pmorrel3@protonmail.com>
2025-08-17 21:48:53 -0400

    Added license to static assets file

--- uplink/ui/src/lib/static-assets.ts
+++ uplink/ui/src/lib/static-assets.ts
@@ -1,4 +1,18 @@
+// Fosstank: 24/7 live streaming platform
+// Copyright (C) 2025 Pierre Morrel
 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 export const STATIC_ASSETS = {
     "avatar": "/static/cbdd90737dfaff80dca5a09925a82e541035242d.jpg",
